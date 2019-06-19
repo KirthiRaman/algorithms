281a186e0394478e4eb773e5aa02905ac31ea69d
Kirthi Raman <kirthi.raman@gmail.com>
2019-06-19 08:29:28 -0400

    Create reverselist.py

---
+++ reverselist.py
@@ -0,0 +1,14 @@
+def reverseString(s):
+        if not s:
+           return
+        slen = len(s)
+        for i in range(0,slen//2):
+            t = s[i]
+            s[i] = s[slen-1-i]
+            s[slen-1-i] = t
+        print("Result =>",s)
+        return
+
+s=["h","e","l","l","o"]
+print("Input =>",s)
+reverseString(s)
